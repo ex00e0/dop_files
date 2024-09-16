886eb7a6d724ad8ccb88f4aea941fcd26729a008
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-09-16 08:59:25 +0300

    Add files via upload

--- todo.sql
+++ todo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 13 2024 г., 21:41
+-- Время создания: Сен 16 2024 г., 10:58
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -30,12 +30,23 @@ SET time_zone = "+00:00";
 CREATE TABLE `tasks` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
-  `title` int NOT NULL,
+  `title` varchar(40) NOT NULL,
   `description` varchar(600) DEFAULT NULL,
-  `is_completed` enum('true','false') NOT NULL,
+  `is_completed` enum('true','false') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'false',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `is_completed`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Задача #1', 'uho', 'false', '2024-09-16 08:58:53', '2024-09-16 08:58:53'),
+(2, 1, 'Задача #2', NULL, 'true', '2024-09-16 09:05:00', '2024-09-16 09:05:00'),
+(3, 1, 'clean', 'steps : at first...\r\nnext..\r\nim tired', 'false', '2024-09-16 09:06:19', '2024-09-16 09:06:19'),
+(4, 1, 'dbfbd', 'bvcv', 'false', '2024-09-16 09:59:29', '2024-09-16 09:59:29'),
+(5, 1, 'bv bgv fgh', NULL, 'false', '2024-09-16 10:32:50', '2024-09-16 10:32:50');
 
 -- --------------------------------------------------------
 
@@ -81,13 +92,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
